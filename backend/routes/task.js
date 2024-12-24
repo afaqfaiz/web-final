600c87c7a84ce350fa09f3b9b685c4eb47e16a74
afaq <aafaqahmadcs@gmail.com>
2024-12-24 14:08:21 +0530

    add the task retrive api

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -4,27 +4,22 @@ const jwt = require('jsonwebtoken');
 const Task = require('../models/task');
 const router = express.Router();
 const dotenv = require('dotenv');
+const connectDB = require('../db');
+const db = connectDB;
 
 dotenv.config();
 
 // Register a new user
 router.post('/store', async (req, res) => {
-  const { title, description, date } = req.body;
+  const { title, description, date} = req.body;
 
   console.log(title);
   console.log(description);
   console.log(date);
 
+
   
   try {
-    // Check if user already exists
-    // const existingUser = await User.findOne({ email });
-    // if (existingUser) return res.status(400).json({ message: 'User already exists' });
-
-    // Hash the password before saving
-    // const hashedPassword = await bcrypt.hash(password, 10);
-    
-    // Create a new user
     const newTask = new Task({
       title,
       description,
@@ -38,5 +33,19 @@ router.post('/store', async (req, res) => {
     res.status(500).json({ message: 'Error adding task', error: error.message });
   }
 });
+
+//http://localhost:5000/api/task/alltask
+
+router.get('/alltask', async (req, res) => {
+    console.log("hhh");
+    try {
+        const fulldata = await Task.find();
+  
+      // Save the user to the database
+      res.status(201).json(fulldata);
+    } catch (error) {
+      res.status(500).json({ message: 'Error finding  task', error: error.message });
+    }
+  });
 
 module.exports = router;
